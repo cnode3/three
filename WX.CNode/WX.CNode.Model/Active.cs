8e324d246015b3f1bf7f3a41d5f516a78fa40488
lijiaqi <ljqedit@163.com>
2019-01-29 15:10:12 +0800

    123
    123

--- WX.CNode/WX.CNode.Model/Active.cs
+++ WX.CNode/WX.CNode.Model/Active.cs
@@ -24,7 +24,37 @@ namespace WX.CNode.Model
         /// 类别ID
         /// </summary>
         public int TypeID { get; set; }
-       
-
+        /// <summary>
+        /// 类别名称
+        /// </summary>
+        public string TypeName { get; set; }
+        /// <summary>
+        /// 浏览量ID
+        /// </summary>
+        public int BrowserID { get; set; }
+        /// <summary>
+        /// 浏览量
+        /// </summary>
+        public int ActiveNum { get; set; }
+        /// <summary>
+        /// 发布日期
+        /// </summary>
+        public DateTime PublishDate { get; set; }
+        /// <summary>
+        /// 用户表ID
+        /// </summary>
+        public int UserID { get; set; }
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 图片表ID
+        /// </summary>
+        public int ImgID { get; set; }
+        /// <summary>
+        /// 图片路径
+        /// </summary>
+        public string ImgUrl { get; set; }
     }
 }
