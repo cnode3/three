b51a19cec94b8f4347b6733f582c2a31ccfdb1d4
lijiaqi <ljqedit@163.com>
2019-01-29 14:54:00 +0800

    2
    2

--- WX.CNode/WX.CNode.Model/Active.cs
+++ WX.CNode/WX.CNode.Model/Active.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace WX.CNode.Model
 {
-    public class Class1
+    public class Active
     {
+        /// <summary>
+        /// 动态表主键ID
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Content { get; set; }
+        /// <summary>
+        /// 类别ID
+        /// </summary>
+        public int TypeID { get; set; }
+       
+
     }
 }
